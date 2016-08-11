3d2bfbc9a6c1a8e57951381c5d3c19834ea59c53
iMac <imac@iMacs-iMac.local>
2016-08-11 09:28:01 +0200

    added twitter widget

--- jobs/test.rb
+++ jobs/test.rb
@@ -1,7 +1,12 @@
+start_time = Time.new
+start_time = start_time.min
+
 # :first_in sets how long it takes before the job is first run. In this case, it is run immediately
-SCHEDULER.every '1s', :first_in => 0 do |job|
-  send_event('response_time1', {
-    value: (rand * 600).to_i
+SCHEDULER.every '2s', :first_in => 0 do |job|
+  current_time = Time.new
+  current_time = current_time.min
+  send_event('up_time', {
+    value: current_time - start_time
   })
   send_event('response_time2', {
     value: (rand * 600).to_i
